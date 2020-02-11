ec4b8e3f9608dc118856ca49b8ab813e76e30d75
PanGalacticTech <53580358+PanGalacticTech@users.noreply.github.com>
2020-02-11 18:55:13 +0000

    Project for learning and testing basic javascript functions

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+// My First Function
+// Performing A Task
+function greet(name, lastName) {   // paramiter to a function
+
+ console.log('Hello ' + name + ' ' + lastName);
+
+}    // no semicolon here
+
+
+
+
+greet('Johnathon', 'Creek');   // argument passed to function
+greet('Alice', 'inChains');   // argument passed to function
+
+
+
+// Calculating a Value
+
+function square(number){
+return number*number;
+}
+
+//let number = square(2);
+
+console.log(square(2));
